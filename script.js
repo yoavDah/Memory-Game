ccae75b5ab4676983bc1f4af7e00280d3399a263
yoav <yoavdah@gmail.com>
2026-06-17 02:10:34 +0300

    add logic to get the pictures

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const arrayOfAPI = ['https://dog.ceo/api/breeds/image/random','https://api.thecatapi.com/v1/images/search?limit=10' , 'https://hp-api.onrender.com/api/characters' ]
-let chosenTheme = null
+const arrayOfAPI = ['https://dog.ceo/api/breed/hound/images','https://api.thecatapi.com/v1/images/search?limit=6' , 'https://hp-api.onrender.com/api/characters' ]
+const arrayOfImages = []
+const body = document.querySelector('body')
+let chosenTheme;
 let indexOfSelectedApi;
 
-async function greet() {
-  return "Hello!"; 
-}
 
-console.log(greet());
-greet().then(dada => console.log(dada));
 
 
 document.addEventListener('DOMContentLoaded', () =>{
@@ -15,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () =>{
     buttons.forEach(button => button.addEventListener('click', (e) =>{
     const selectedButton = e.target
     indexOfSelectedApi = e.target.dataset.button
+    
+
 
         if (indexOfSelectedApi == 'random') {
             generateRandomNumber()
@@ -38,12 +37,36 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
 
 
+
+
+    const getPictures = (data) => {
+        for (let i = 0; i < 7; i++){
+            if (chosenTheme == arrayOfAPI[0]){
+                console.log(data.message[i]);
+                arrayOfImages.push(data.message[i])
+            } else if (chosenTheme == arrayOfAPI[1]){
+                console.log(data[i].url);    
+                arrayOfImages.push(data[i].url)
+            } else if (chosenTheme == arrayOfAPI[2]){
+                console.log(data[i].image);
+                arrayOfImages.push(data[i].image)
+            }
+        }
+       generateBoard() 
+    }
+
+    const generateBoard = () =>{
+        console.log(arrayOfImages);
+        body.innerHTML = 
+    }
+
+
     const getImagesFromAPI = ()  =>{
         console.log(chosenTheme);
         
         fetch(chosenTheme)
           .then(response => response.json())
-          .then(data => console.log(data))
+          .then(data => getPictures(data))
     }
 
 })
